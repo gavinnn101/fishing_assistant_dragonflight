9e782c8e90b99e315b8e83e828f7f3eb1e288d64
gavinnn101 <41026935+gavinnn101@users.noreply.github.com>
2023-01-12 01:12:41 -0800

    deposits all non-soulbound items (no reagant bag, no full tab check)

--- utility/GBankDeposit/GBankDeposit.lua
+++ utility/GBankDeposit/GBankDeposit.lua
@@ -1,3 +1,40 @@
+local delay = 1 -- delay in seconds
+
+-- Define the function that deposits items
+local function depositItem(bag, slot)
+    local itemID = C_Container.GetContainerItemID(bag, slot)
+    if itemID then
+        -- Skip item if it's soulbound
+        if C_Item.IsBound(ItemLocation:CreateFromBagAndSlot(bag, slot)) then
+            print("Skipping soulbound item: " ..itemID)
+        else
+            print("Interacting with item: " ..itemID)
+            -- Pickup item
+            C_Container.UseContainerItem(bag, slot)
+            print("finished interacting with item: " ..itemID)
+        end
+        -- Find the next item to deposit
+        for i = slot + 1, C_Container.GetContainerNumSlots(bag) do
+            if C_Container.GetContainerItemID(bag, i) then
+                -- Deposit the next item after the delay
+                C_Timer.After(delay, function() depositItem(bag, i) end)
+                return
+            end
+        end
+        for i = 0, NUM_BAG_SLOTS do
+            if i ~= bag then
+                for j = 1, C_Container.GetContainerNumSlots(i) do
+                    if C_Container.GetContainerItemID(i, j) then
+                        -- Deposit the next item after the delay
+                        C_Timer.After(delay, function() depositItem(i, j) end)
+                        return
+                    end
+                end
+            end
+        end
+    end
+end
+
 -- Create a frame to listen for the PLAYER_INTERACTION_MANAGER_FRAME_SHOW event
 local frame = CreateFrame("Frame")
 frame:RegisterEvent("PLAYER_INTERACTION_MANAGER_FRAME_SHOW");
@@ -7,31 +44,13 @@ function frame:OnEvent(event, arg1)
     if event == "PLAYER_INTERACTION_MANAGER_FRAME_SHOW" then
         if arg1 == 10 then --Guild Bank
             print("GBank opened")
-            -- Iterate over all bags
+            -- Find the first item to deposit
             for bag = 0, NUM_BAG_SLOTS do
-                -- Iterate over all slots in the current bag
                 for slot = 1, C_Container.GetContainerNumSlots(bag) do
-                    -- Get item ID
-                    local itemID = C_Container.GetContainerItemID(bag, slot)
-                    print("itemID: " ..itemID)
-                    -- Skip item if it's soulbound
-                    if C_Item.IsBound(ItemLocation:CreateFromBagAndSlot(bag, slot)) then
-                        print("Skipping soulbound item.")
-                    else
-                        print("Picking up non-soulbound item")
-                        -- Pickup item after 2 seconds
-                        success = C_Timer.After(2, C_Container.PickupContainerItem(bag, slot))
-                        print("success: " ..success)
-                        if not success then
-                            print("Error picking up item: "..itemID.." from bag "..bag..", slot "..slot)
-                        else
-                            print("Depositing item: "..itemID.." to guild bank")
-                            local tab = GetCurrentGuildBankTab()
-                            local success = DepositGuildBankItem(tab, slot)
-                            if not success then
-                                print("Error depositing item: "..itemID.." to guild bank")
-                            end
-                        end
+                    if C_Container.GetContainerItemID(bag, slot) then
+                        -- Deposit the first item
+                        depositItem(bag, slot)
+                        return
                     end
                 end
             end
