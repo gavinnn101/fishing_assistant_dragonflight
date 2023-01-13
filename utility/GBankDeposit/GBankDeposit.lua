61a3de344843f8af4a4d7204c174c8633c502141
gavinnn101 <41026935+gavinnn101@users.noreply.github.com>
2023-01-12 23:34:28 -0800

    correctly find a bank tab with available space

--- utility/GBankDeposit/GBankDeposit.lua
+++ utility/GBankDeposit/GBankDeposit.lua
@@ -35,6 +35,23 @@ local function depositItem(bag, slot)
     end
 end
 
+local function findAvailableBankTab()
+    print("Finding bank tab with available space")
+    local numTabs = GetNumGuildBankTabs()
+    while numTabs > 0 do
+        -- If the last slot (98) is available then we can deposit our item.
+        local bankTab = GetCurrentGuildBankTab()
+        local lastSlot = GetGuildBankItemInfo(bankTab, 98)
+        if lastSlot == nil then
+            print("Found guild bank slot with available space: " ..numTabs)
+            return true
+        end
+        SetCurrentGuildBankTab(numTabs)
+        numTabs = numTabs - 1
+    end
+    return false
+end
+
 -- Create a frame to listen for the PLAYER_INTERACTION_MANAGER_FRAME_SHOW event
 local frame = CreateFrame("Frame")
 frame:RegisterEvent("PLAYER_INTERACTION_MANAGER_FRAME_SHOW");
@@ -44,15 +61,22 @@ function frame:OnEvent(event, arg1)
     if event == "PLAYER_INTERACTION_MANAGER_FRAME_SHOW" then
         if arg1 == 10 then --Guild Bank
             print("GBank opened")
-            -- Find the first item to deposit
-            for bag = 0, NUM_BAG_SLOTS do
-                for slot = 1, C_Container.GetContainerNumSlots(bag) do
-                    if C_Container.GetContainerItemID(bag, slot) then
-                        -- Deposit the first item
-                        depositItem(bag, slot)
-                        return
+            -- Find bank tab with available space
+            if findAvailableBankTab() then
+                -- Loop over each bag on character
+                for bag = BACKPACK_CONTAINER, NUM_TOTAL_EQUIPPED_BAG_SLOTS do
+                    -- Loop over each slot in bag to find an item
+                    for slot = 1, C_Container.GetContainerNumSlots(bag) do
+                        local itemID = C_Container.GetContainerItemID(bag, slot)
+                        if itemID then
+                            print("Depositing item: " ..itemID.. "in bag: " ..bag)
+                            depositItem(bag, slot)
+                            return
+                        end
                     end
                 end
+            else
+                print("You don't have any available space in your guild bank!!!")
             end
         end
     end
